d13c3ee4dcd3b16fa56908bcc13a12ba5f1df856
Nikhil Anand <nikhilanandgaya01@gmail.com>
2021-03-19 11:45:12 +0530

    network folder

---
+++ network/chat/client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 55555
+nickname = input("Choose a Nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An Error Occurred")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
