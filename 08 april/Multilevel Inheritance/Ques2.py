6e41ca476bd4eb0db07e1bdb07de7d3ca3cb0535
niksanand1717 <nikhilanandgaya01@gmail.com>
2021-04-08 19:37:03 +0530

    multilevel Inheritance

---
+++ 08 april/Multilevel Inheritance/Ques2.py
@@ -0,0 +1,37 @@
+class Student():
+    name = str()
+    studentID = str()
+    universityRN = str()
+
+    def getData(self, student_name, student_ID, university_roll_no):
+        self.name = student_name
+        self.studentID = student_ID
+        self.universityRN = university_roll_no
+
+class Grade(Student):
+    marks = []
+    def getMarks(self):
+        print("Enter the marks of 5 subjects separated by space: ")
+        for i in range(5):
+            self.marks.append(float(input()))
+
+
+class FinalResult(Grade):
+    average = 0.0
+
+    def avg(self, marks):
+        return sum(self.marks) / len(self.marks)
+
+    def findAverage(self):
+        self.average = self.avg(self.marks)
+
+    def ResultDisplay(self):
+        string = "\n\nName: "+self.name+"\nStudent ID: "+self.studentID+"\nUniversity Roll Number: "+self.universityRN+"\nTotal Marks: "+str(sum(self.marks))+"\nAverage: "+str(self.average)
+        print(string)
+
+
+student1 = FinalResult()
+student1.getData(student_name="Nikhil Anand", student_ID="19011717", university_roll_no="1918508")
+student1.getMarks()
+student1.findAverage()
+student1.ResultDisplay()
